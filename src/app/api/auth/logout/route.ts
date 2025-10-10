d425c1caa0486b652d426a1a3a26685f2abd9d60
athalafk <athalafarastya@gmail.com>
2025-10-11 02:40:57 +0700

    fix: bugs logout

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -4,5 +4,5 @@ import { createClient } from "@/utils/supabase/server";
 export async function POST() {
   const supabase = createClient();
   await (await supabase).auth.signOut();
-  return NextResponse.redirect(new URL("/login", process.env.NEXT_PUBLIC_BASE_URL ?? "http://localhost:3000"));
+  return NextResponse.redirect(new URL("/login", process.env.NEXT_PUBLIC_BASE_URL));
 }
